f9810092e6b62342f9f96f41775c52f3c3e24521
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-05-03 17:05:01 +0200

    Footer responsive
    Footer
    >Responsive

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -10,24 +10,27 @@ function Footer() {
                     <img src="https://res.cloudinary.com/dz0mwxb0v/image/upload/v1714402089/eLearning/logo_eLearning.png" alt="Logo" />
                     <h2>eLearning</h2>
                 </div>
-                <div className="footer-section about">
-                    <h3>ABOUT</h3>
-                    <ul>
-                        <li><a href="#team">Team</a></li>
-                        <li><a href="#reviews">Reviews</a></li>
-                        <li><a href="#refunds">Refunds</a></li>
-                        <li><a href="#roadmap">Roadmap</a></li>
-                    </ul>
+                <div className="menus">
+                    <div className="footer-section about">
+                        <h3>ABOUT</h3>
+                        <ul>
+                            <li><a href="#team">Team</a></li>
+                            <li><a href="#reviews">Reviews</a></li>
+                            <li><a href="#refunds">Refunds</a></li>
+                            <li><a href="#roadmap">Roadmap</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-section community">
+                        <h3>COMMUNITY</h3>
+                        <ul>
+                            <li><a href="#podcast">Podcast</a></li>
+                            <li><a href="#forum">Forum</a></li>
+                            <li><a href="#discord">Discord</a></li>
+                            <li><a href="#groups">Groups</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="footer-section community">
-                    <h3>COMMUNITY</h3>
-                    <ul>
-                        <li><a href="#podcast">Podcast</a></li>
-                        <li><a href="#forum">Forum</a></li>
-                        <li><a href="#discord">Discord</a></li>
-                        <li><a href="#groups">Groups</a></li>
-                    </ul>
-                </div>
+
                 <div className="footer-section contact">
                     <h3>CONTACT</h3>
                     <p><a href="#">hola@brian-novoa.com</a></p>
